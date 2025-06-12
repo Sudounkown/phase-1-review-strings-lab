ac8d51a81d851b2f4f75cf7755042ac99dbb2915
Roy Koigu <roykoigu@protonmail.com>
2025-06-12 05:44:07 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = "Someone"
+let welcomeMessage = `${currentUser} "Welcome to Flatbook, !`
+const upperCaseWelcomMessage = currentUser.toUpperCase()
+let excitedWelcomeMessage = ` ${upperCaseWelcomMessage} WELCOME TO FLATBOOK, !`
+const firstInitial = currentUser[0]
+const restOfName = currentUser.slice(1)
+
+let shortGreeting = `Welcome,  ${firstInitial} !`
